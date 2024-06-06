a55eec13a59caa4457341b330e090da12dc4fef0
alfredoperry14 <alfredoperry14@gmail.com>
2024-06-05 18:11:51 -0700

    Finished the app with full functionality

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,61 +9,111 @@ import SwiftUI
 
 let rpsChoices: [String] = ["🪨","📄","✂️"]
 let winLose: [String] = ["Win", "Lose"]
-var score = 0
 
 
-struct cpuTitle: View{
+struct cpuChoiceView: View{
     var cpuChoice: String
     var body: some View{
         Text(cpuChoice)
-            .font(.custom("Test", size: 80))
+            .font(.custom("cpuChoice", size: 80))
+    }
+}
+
+struct playerChoiceView: View {
+    var rps: String
+    var body: some View {
+        Text(rps)
+            .frame(maxWidth: .infinity)
+            .font(.custom("playerChoice", size: 70))
+            .background(
+                Circle()
+                    .padding(-10)
+            )
     }
 }
 
 struct ContentView: View {
+    @State private var score = 0
+    @State private var scoreTitle = ""
+    @State private var reset = false
+    
+    @State var prevRPS = "none"
+    @State var rpsChoice = rpsChoices[Int.random(in: 0...2)]
+    @State var winLoseChoice = winLose[Int.random(in: 0...1)]
+    @State var questions = 0
+    
+    
+    
     var body: some View {
+        
         //Hardcoding the random values because these inputs should never change
-        var rpsChoice = rpsChoices[Int.random(in: 0...2)]
-        var winLoseChoice = winLose[Int.random(in: 0...1)]
         VStack {
-            Text("Choose the correct option \nthat completes the scenario")
+            Text("Choose the correct option that completes the scenario")
+                .font(.title)
+                .multilineTextAlignment(.center)
             Spacer()
+            
             VStack{
-                cpuTitle(cpuChoice: "\(winLoseChoice)")
+                cpuChoiceView(cpuChoice: "\(winLoseChoice)")
                     .foregroundStyle(winLoseChoice == "Win" ? .green : .red)
-                cpuTitle(cpuChoice: "\(rpsChoice)")
+                cpuChoiceView(cpuChoice: "\(rpsChoice)")
             }
             Spacer()
-
-            buttonView()
+            
+            HStack{
+                ForEach(rpsChoices, id: \.self){ rps in
+                    Button{
+                        buttonPressed(playerChoice: "\(rps)")
+                    } label: {
+                        playerChoiceView(rps: "\(rps)")
+                    }
+                    .foregroundStyle(winLoseChoice == "Win" ? .green : .red)
+                }
+                
+            }
             
             Spacer()
-            Text("\(score)")
+            Text("Score: \(score)")
+                .font(.title2)
+                .bold()
+            
         }
-        .padding()
+        
+        .alert(scoreTitle, isPresented: $reset){
+            Button("Reset", action: resetGame)
+        } message: {
+            Text("You got \(score) out of 100")
+        }
+        
     }
     
-    
-    struct buttonView: View {
-        var correctAnswer: String
-        var body: some View {
-            HStack{
-                ForEach(rpsChoices, id: \.self){ rpsChoice in
-                    Button{
-                        buttonPressed(rpsChoice: "hey", playerChoice: "hey")
-                    } label: {
-                        Text("\(rpsChoice)")
-                    }
-                }
-            }
-        }
+    func resetGame(){
+        score = 0
+        questions = 0
+        randomizeRPS()
     }
     
-    func buttonPressed(rpsChoice: String, playerChoice: String){
-        if(rpsChoice == playerChoice){
+    func buttonPressed(playerChoice: String){
+        let correctChoice = correctAnswer(rpsChoice, scenario: winLoseChoice)
+        if(correctChoice == playerChoice){
             score += 10
         }
-            
+        
+        if(questions == 9){
+            reset = true
+        }
+        questions += 1
+        
+        randomizeRPS()
+    }
+    
+    //Randomizes the rock paper scissors scenario
+    func randomizeRPS(){
+        prevRPS = rpsChoice
+        while(prevRPS == rpsChoice){
+            rpsChoice = rpsChoices[Int.random(in: 0...2)]
+            winLoseChoice = winLose[Int.random(in: 0...1)]
+        }
     }
     
     func correctAnswer(_ rpsChoice:String, scenario winLose: String ) -> String{
